e12d20a9c8fe8a748158058438e1b6c6d1fc24da
MohamedAbdulsalam96 <moha2016it@gmail.com>
2025-04-24 19:54:25 +0200

    log_gen.py

--- whatsapp/whatsapp/log_gen.py
+++ whatsapp/whatsapp/log_gen.py
@@ -209,73 +209,167 @@ def process_report_whatsapp_pop_up(data):
         # from frappe.templates.pages.print import print_report
         # from frappe.utils.print_format import print_report
 
-#         template_path = os.path.join(
-#     frappe.get_app_path("frappe"), "public", "html", "print_template.html"
-# )
-#         with open(template_path, "r", encoding="utf-8") as f:
-#                 base_html = f.read()
-
-        # frappe.msgprint("here")
-        # def safe_translate(string, args=None):
-        #         if isinstance(args, (list, tuple)):
-        #                 return string.format(*args)
-        #         return string
-        # # Create context for the Jinja template
-        # context = {
-        #         "title": "Custom Report",
-        #         "base_url": frappe.utils.get_url(),
-        #         # "print_css": frappe.get_print_style(),
-        #         "lang": frappe.local.lang,
-        #         "layout_direction": "rtl" if frappe.local.lang == "ar" else "ltr",
-        #         "landscape": True,
-        #         "columns": [{"label": "Name"}, {"label": "Amount"}],
-        #         "can_use_smaller_font": True,
-        #         "content": "",
-        #         "print_settings": {
-        #                 "letter_head": {
-        #                         "header": "<p>Header from Letterhead</p>",
-        #                         "footer": "<p>Footer here</p>"
-        #                 },
-        #                 "repeat_header_footer": 1
-        #         },
-        #         "__": safe_translate
-        # }
-        # Render final HTML
-        # final_html = frappe.render_template(base_html, context)
-
-
-
-#         pdf_content = build_report_pdf(
-#         report_name="",
-#         filters=[],
-#         letterhead="",
-#         landscape=True
-#     )
-#         frappe.msgprint(f"file doc saved")
-
-        # context = {
-        #         "letter_head": frappe.get_doc("Letter Head", letter_head).as_dict() if letter_head else None,
-        #         "data": {
-        #         "columns": columns,
-        #         "data": rows,
-        #         },
-        #         "lang": frappe.local.lang,
-        #         "layout_direction": "rtl" if is_rtl() else "ltr"
-        # }
-
-        # html = frappe.render_template("whatsapp/templates/includes/report_pdf.html", context)
-        # pdf = get_pdf(html, {"orientation": orientation})
-
-        # file_doc = frappe.get_doc({
-        #         "doctype": "File",
-        #         "file_name": f"Report-{frappe.utils.now_datetime()}.pdf",
-        #         "is_private": 1,
-        #         "content": pdf
-        # })
-        # file_doc.save(ignore_permissions=True)
-        # return file_doc.file_url
                 
-                
+from frappe.utils import now_datetime
+from frappe.utils.data import time_diff_in_seconds
+from frappe.utils import getdate
+
+
+# from frappe.utils import time_diff_in_minutes
+
+
+def months_apart(date1, date2):
+        """Return number of full months between two dates"""
+        print("months_apart")
+        d1 = getdate(date1)
+        d2 = getdate(date2)
+        return (d2.year - d1.year) * 12 + d2.month - d1.month
+
+
+@frappe.whitelist()
+def process_scheduled_rule():
+        rules = frappe.get_all("WA Automation Rule", filters={
+                "enabled": 1,
+                "scheduled": 1
+        }, fields=['*'])
+
+        for rule in rules:
+                if should_run(rule):
+                        module = frappe.get_meta(rule.document_type).module
+                        print(f"rule{rule}")
+
+
+                        doc = frappe.get_single("WA Setting") 
+                        account=""
+                        for row in doc.account_module_settings:
+                                if row.module ==module:
+                                        account= row.sending_account 
+
+                        
+                        if not account:
+                                frappe.throw(f"Account is not set for module {module} in WA Setting")
+                        print("should run")
+                        #generate pdf
+                        report = frappe.get_doc("Report", rule.report_ref)
+                        columns, data = report.get_data(
+                        user="Administrator",  # or current user
+                        # filters=filters,
+                        as_dict=True,
+                        ignore_prepared_report=True
+                                )
+                        grid_html = frappe.render_template("frappe/public/js/frappe/views/reports/print_grid.html", {
+                                "title": report.name,
+                                "subtitle": "",
+                                "columns": columns,
+                                "data": data,
+                                "original_data": data,
+                                "landscape": True,
+                                "can_use_smaller_font": True,
+                                "report": report
+                        })
+                        full_html = frappe.render_template("frappe/templates/print_formats/standard.html", {
+                                "title": report.name,
+                                "content": grid_html,
+                                "base_url": frappe.utils.get_url(),
+                                "print_css": frappe.get_print_style("Standard", as_dict=False),
+                                "lang": frappe.local.lang,
+                                "layout_direction": "rtl" if frappe.local.lang in ["ar", "ur", "he"] else "ltr",
+                                "landscape": True,
+                                "columns": columns,
+                                "can_use_smaller_font": True,
+                                "print_settings": {
+                                        "letter_head": {
+                                                "header": "<p>Header</p>",
+                                                "footer": "<p>Footer</p>"
+                                        },
+                                        "repeat_header_footer": 1
+                                }
+                        })
+
+
+                        
+                        pdf_content = frappe.utils.pdf.get_pdf(full_html)
+
+
+
+                        print(f"hrml{full_html}")
+                        # pdf_content = frappe.utils.pdf.get_pdf(html)
+                        print("PDF length:", len(pdf_content))
+
+                        # save as File doc
+                        print("no error")
+                        file_doc = frappe.get_doc({
+                        "doctype": "File",
+                        "file_name": f"Reportt-{frappe.utils.now_datetime()}.pdf",
+                        "is_private": 1,
+                        "content": pdf_content
+                        })
+                        file_doc.save(ignore_permissions=True)
+                        if file_doc:
+                                log_created=generate_log(account,rule.recipient,"Send Attach",rule.document_type,doc,"Direct","",file_doc.file_url)
+                                if log_created:
+                                        frappe.db.set_value("WA Automation Rule", rule.name, "last_run", now_datetime())
+                        #         print("file_doc")
+                        # file_doc = frappe.get_doc({
+                        # "doctype": "File",
+                        # "file_name": f"{report.name}.pdf",
+                        # "content": pdf_content,
+                        # "is_private": 1
+                        # }).insert(ignore_permissions=True)
+
+                        #save it as file_doc
+                       
+        
+def should_run(rule):
+        last_run = rule.get("last_run")
+        now = now_datetime()
+
+        if not last_run:
+                last_run = rule.get("start_date")
+
+        if rule.schedule_interval == "Monthly":
+                return months_apart(last_run, now) >= 1
+        
+
+        diff_minutes = time_diff_in_seconds(now, last_run) / 60
+        print(f"diff_minutes{diff_minutes}")
+
+        interval = get_interval_in_minutes(rule.schedule_interval)
+        return diff_minutes >= interval
+
+def get_interval_in_minutes(schedule_type):
+        print(f"get_interval_in_minutes{schedule_type}")
+        if schedule_type == "Daily":
+                print("daily")
+
+                return 1440
+        if schedule_type == "Weekly":
+                return 10080
+        if schedule_type == "Yearly":
+                return 525600  # 365 * 24 * 60
+        return 9999999  # fallback for others
+
+
+def months_apart(date1, date2):
+	"""Return number of full months between two dates"""
+	d1 = getdate(date1)
+	d2 = getdate(date2)
+	return (d2.year - d1.year) * 12 + d2.month - d1.month
+
+
+def should_trigger(schedule_type, diff):
+        if schedule_type == "Every Minute" and diff >= 1:
+                return True
+        if schedule_type == "Every 2 Minutes" and diff >= 2:
+                return True
+        if schedule_type == "Hourly" and diff >= 60:
+                return True
+        if schedule_type == "Daily" and diff >= 1440:
+                return True
+        # Add more as needed
+        return False
+
+               
 
                         
 def generate_log(sender,reciever,type,doctype,ref_doc,channel_type,content,url=None):
@@ -285,7 +379,7 @@ def generate_log(sender,reciever,type,doctype,ref_doc,channel_type,content,url=N
         
                 
 
-        frappe.get_doc(
+        doc=frappe.get_doc(
                         {
                                 "doctype": "WA Log",
                         
@@ -306,5 +400,7 @@ def generate_log(sender,reciever,type,doctype,ref_doc,channel_type,content,url=N
                         }
                 ).insert() 
                 
+        if doc:
+                return True
 
 # return "Salary slips generated and saved."
